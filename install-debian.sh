09d70b390cf004a443e4e203040133c7a80147ca
odiak <158767461+odiak0@users.noreply.github.com>
2024-03-13 21:21:57 +0100

    Update install-debian.sh

--- install-debian.sh
+++ install-debian.sh
@@ -15,6 +15,7 @@ sudo rm -fv /etc/apt/sources.list.d/thorium.list && \
 sudo wget --no-hsts -P /etc/apt/sources.list.d/ \
 http://dl.thorium.rocks/debian/dists/stable/thorium.list && \
 sudo apt update
+sudo apt install thorium-browser
 
 ### Installing theme ###
 
