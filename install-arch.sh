3b49affb2add39bbef8a04e046edcd4e68413a82
odiak <158767461+odiak0@users.noreply.github.com>
2024-03-18 22:25:21 +0100

    Add files via upload

---
+++ install-arch.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+### Installing yay ###
+
+cd
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+rm -rf ~/yay
+
+### Updating system ###
+
+sudo pacman -Syu
+
+### Installing packages ###
+
+yay -S --noconfirm feh btop kitty rofi bspwm sxhkd polybar gvfs thunar thunar-archive-plugin thunar-media-tags-plugin thunar-volman lxsession unzip wget curl pipewire wireplumber pavucontrol xarchiver base-devel linux-headers neofetch mangohud neovim lxappearance papirus-icon-theme lightdm psmisc dunst
+
+### Installing theme ###
+
+cd /usr/share/themes/
+sudo git clone https://github.com/EliverLara/Nordic.git
+cd
+
+### Enable lightdm ###
+
+sudo systemctl enable lightdm
+sudo systemctl set-default graphical.target
+
+### Moving configs ###
+
+cd ~/bspwm-config/
+mkdir ~/.config/bspwm
+mv -vf ~/bspwm-config/bspwm/bspwmrc ~/.config/bspwm
+chmod +x ~/.config/bspwm/bspwmrc
+mkdir ~/.config/sxhkd
+mv -vf ~/bspwm-config/sxhkd/sxhkdrc ~/.config/sxhkd
+chmod +x ~/.config/sxhkd/sxhkdrc
+mkdir ~/.config/kitty
+mv -vf ~/bspwm-config/kitty/kitty.conf ~/.config/kitty
+mkdir ~/.config/rofi
+mv -vf ~/bspwm-config/rofi/config.rasi ~/.config/rofi
+mkdir ~/wallpaper
+mv -vf ~/bspwm-config/wallpaper/mazda.jpg ~/wallpaper
+sudo mv -vf ~/bspwm-config/polybar/config.ini /etc/polybar
+
+GREEN='\033[0;32m'
+printf "\n${GREEN} Now you can reboot!\n"
