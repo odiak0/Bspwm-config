81fce1651d6b2b00d55479e82b69daf0fa36b54d
odiak <158767461+odiak0@users.noreply.github.com>
2024-04-06 23:59:14 +0200

    asking for install or move

--- install-arch.sh
+++ install-arch.sh
@@ -14,7 +14,17 @@ sudo pacman -Syu
 
 ### Installing packages ###
 
+read -rep "Would you like to install the packages? (y/n)" pkgs
+echo
+
+if [[ $pkgs =~ ^[Nn]$ ]]; then
+    printf "No packages installed. \n"
+fi
+
+if [[ $pkgs =~ ^[Yy]$ ]]; then
+    printf "Installing packages. \n"
 yay -S --noconfirm --needed feh btop kitty xdg-desktop-portal-gtk fuse2 noto-fonts noto-fonts-emoji ttf-caladea ttf-carlito ttf-cascadia-code ttf-dejavu ttf-liberation thorium-browser-bin lightdm-gtk-greeter rofi bspwm sxhkd polybar gvfs thunar thunar-archive-plugin thunar-media-tags-plugin thunar-volman lxsession unzip wget curl pipewire wireplumber pavucontrol xarchiver base-devel linux-headers neofetch mangohud neovim lxappearance papirus-icon-theme lightdm psmisc dunst
+fi
 
 ### Installing theme ###
 
@@ -29,6 +39,15 @@ sudo systemctl set-default graphical.target
 
 ### Moving configs ###
 
+read -rep "Would you like to move the configs? (y/n)" configs
+echo
+
+if [[ $configs =~ ^[Nn]$ ]]; then
+    printf "No configs moved. \n"
+fi
+
+if [[ $configs =~ ^[Yy]$ ]]; then
+	printf "Moving configs. \n"
 cd ~/bspwm-config/
 mkdir ~/.config/bspwm
 mv -vf ~/bspwm-config/bspwm/bspwmrc ~/.config/bspwm
@@ -43,6 +62,7 @@ mv -vf ~/bspwm-config/rofi/config.rasi ~/.config/rofi
 mkdir ~/wallpaper
 mv -vf ~/bspwm-config/wallpaper/* ~/wallpaper
 sudo mv -vf ~/bspwm-config/polybar/config.ini /etc/polybar
+fi
 
 GREEN='\033[0;32m'
 printf "\n${GREEN} Now you can reboot!\n"
